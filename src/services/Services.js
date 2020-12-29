91d30779aceb23c31aa7cb6e80745055ceb14bfb
Anthony Santo <emailpapasanto@gmail.com>
2020-12-29 08:27:20 -0500

    fixed card for andriod

--- src/services/Services.js
+++ src/services/Services.js
@@ -17,17 +17,19 @@ const Services = () => {
             <li className="services__list--item">Home</li>
             <li className="services__list--item">Commercial</li>
             <li className="services__list--item">Life</li>
-            <li className="services__list--item">Disability</li>
             <li className="services__list--item">Critical</li>
-
-            <li className="services__list--item">RESP & TFSA</li>
+            <li className="services__list--item">Disability</li>
+            <li className="services__list--item">RESP/TFSA</li>
             <li className="services__list--item">Travel</li>
           </ul>
 
           <div className="services__contact">
             <div className="header__right--section">
-              <a className="header__right--phone" href="tel:519-601-6666">
-                <div className="header__right--phone--icon">
+              <a
+                className="header__right--phone services__neg-margin--bottom"
+                href="tel:519-601-6666"
+              >
+                <div className="header__right--phone--icon services__neg-margin--right">
                   <AiOutlinePhone />
                 </div>
                 <div className="header__right--phone--number">519-601-6666</div>
@@ -39,7 +41,7 @@ const Services = () => {
                 className="header__right--email"
                 href="mailto:paulbrar69@ontins.com?cc=paulbrar69@gmail.com"
               >
-                <div className="header__right--email--icon">
+                <div className="header__right--email--icon services__neg-margin--right">
                   <AiOutlineMail />
                 </div>
                 <div className="header__right--email--address">
