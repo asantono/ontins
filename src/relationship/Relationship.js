823eebf44c0fddac48d419337a14a66e272bbe10
Anthony Santo <emailpapasanto@gmail.com>
2021-02-13 09:52:41 -0600

    updated style of relationship cards

--- src/relationship/Relationship.js
+++ src/relationship/Relationship.js
@@ -9,18 +9,21 @@ const Relationship = () => {
       <div className="relationship__title">Putting You First</div>
       <div className="relationship__container">
         <RelationshipCard
+          styling="relationship__card--1"
           title="SUPPORT"
           subtitle="Help When You Need It"
           text="A phone call away from professional insight with years of experience"
           icon={<GiVibratingSmartphone />}
         />
         <RelationshipCard
+          styling="relationship__card--2"
           title="EXPERIENCE"
           subtitle="To ensure your goals are met"
           text="Using years of experience to create the right plan to assure satisfaction"
           icon={<FaRegHandshake />}
         />
         <RelationshipCard
+          styling="relationship__card--3"
           title="LIFESTYLE"
           subtitle="Specialized to fit your needs"
           text="Bringing you products that are built for you and your individual needs"
--- src/relationship/RelationshipCard.js
+++ src/relationship/RelationshipCard.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { GiVibratingSmartphone } from "react-icons/gi";
-import { FaRegHandshake } from "react-icons/fa";
 
 const RelationshipCard = (props) => {
-  const { title, subtitle, text, icon } = props;
+  const { title, subtitle, text, icon, styling } = props;
   return (
-    <div className="relationship-card">
+    <div className={`relationship-card ${styling}`}>
       <div className="relationship-card__icon">{icon}</div>
       <div className="relationship-card__title">{title}</div>
       <div className="relationship-card__subtitle">{subtitle}</div>
